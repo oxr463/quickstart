f58a91168e006506e44267d277d52aa3569bb40f
Lucas Ramage <oxr463@gmx.us>
2020-02-26 14:25:36 -0500

    Update fetcher.sh

--- modules/fetcher.sh
+++ modules/fetcher.sh
@@ -3,28 +3,28 @@
 set -eu
 
 get_filename_from_uri() {
-  local uri=$1
+  uri=$1
 
   basename "${1}"
 }
 
 get_path_from_uri() {
-  local uri=$1
+   uri=$1
 
   echo "${uri}" | cut -d / -f 4-
 }
 
 get_protocol_from_uri() {
-  local uri=$1
+  uri=$1
 
   echo "${uri}" | sed -e 's|://.\+$||'
 }
 
 fetch() {
-  local uri=$1
-  local localfile=$2
+  uri=$1
+  localfile=$2
 
-  local protocol=$(get_protocol_from_uri "${uri}")
+  protocol=$(get_protocol_from_uri "${uri}")
   debug fetch "protocol is ${protocol}"
   if $(isafunc "fetch_${protocol}"); then
     fetch_${protocol} "${1}" "${2}"
@@ -35,10 +35,10 @@ fetch() {
 }
 
 fetch_quickstart() {
-  local uri=$1
-  local localfile=$2
+  uri=$1
+  localfile=$2
 
-  local realurl="http://${server_host}:${server_port}/$(get_path_from_uri "${uri}")"
+  realurl="http://${server_host}:${server_port}/$(get_path_from_uri "${uri}")"
   fetch_http_https_ftp "${realurl}" "${localfile}"
 }
 
@@ -58,19 +58,19 @@ fetch_ftp() {
 }
 
 fetch_http_https_ftp() {
-  local uri=$1
-  local localfile=$2
+  uri=$1
+  localfile=$2
 
   debug fetch_http_https_ftp "Fetching URL ${uri} to ${2}"
   spawn "wget -O ${localfile} ${uri}"
-  local wget_exitcode=$?
+  wget_exitcode=$?
   debug fetch_http_https_ftp "exit code from wget was ${wget_exitcode}"
   return ${wget_exitcode}
 }
 
 fetch_file() {
-  local uri=$1
-  local localfile=$2
+  uri=$1
+  localfile=$2
 
   uri=$(echo "${uri}" | sed -e 's|^file://||')
   debug fetch_file "Symlinking local file ${uri} to ${localfile}"
@@ -78,8 +78,8 @@ fetch_file() {
 }
 
 fetch_tftp() {
-  local uri=$1
-  local localfile=$2
+  uri=$1
+  localfile=$2
 
   uri=$(echo "${uri}" | sed -e 's|^tftp://||')
   host=$(echo "${uri}" | cut -d / -f 1)
