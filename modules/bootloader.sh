2983f15fe1312566e5ff2fb150053c654e1f91ef
Lucas Ramage <oxr463@gmx.us>
2020-02-26 13:58:02 -0500

    Update bootloader.sh

--- modules/bootloader.sh
+++ modules/bootloader.sh
@@ -3,7 +3,7 @@
 set -eu
 
 map_device_to_grub_device() {
-  local device=$1
+  device=$1
 
   if [ ! -f "${chroot_dir}/boot/grub/device.map" ]; then
     debug map_device_to_grub_device "device.map doesn't exist...creating"
@@ -13,13 +13,13 @@ map_device_to_grub_device() {
 }
 
 get_kernel_and_initrd() {
-  local kernels=""
+  kernels=""
   for kernel in $(ls -1t --color=no ${chroot_dir}/boot/kernel-*); do
     kernel="$(echo ${kernel} | sed -e 's:^.\+/kernel-:kernel-:')"
     if [ -e "${chroot_dir}/boot/$(echo ${kernel} | sed -e 's:kernel-:initrd-:')" ]; then
-      local initrd="$(echo ${kernel} | sed -e 's:kernel-:initrd-:')"
+      initrd="$(echo ${kernel} | sed -e 's:kernel-:initrd-:')"
     elif [ -e "${chroot_dir}/boot/$(echo ${kernel} | sed -e 's:kernel-:initramfs-:')" ]; then
-      local initrd="$(echo ${kernel} | sed -e 's:kernel-:initramfs-:')"
+      initrd="$(echo ${kernel} | sed -e 's:kernel-:initramfs-:')"
     fi
     if [ -n "${kernels}" ]; then
       kernels="${kernels} ${kernel}|${initrd}"
@@ -32,29 +32,29 @@ get_kernel_and_initrd() {
 
 get_boot_and_root() {
   for mount in ${localmounts}; do
-    local devnode=$(echo ${mount} | cut -d ':' -f1)
-    local mountpoint=$(echo ${mount} | cut -d ':' -f3)
+    devnode=$(echo ${mount} | cut -d ':' -f1)
+    mountpoint=$(echo ${mount} | cut -d ':' -f3)
     if [ "${mountpoint}" = "/" ]; then
-      local root="${devnode}"
+      root="${devnode}"
     elif [ "${mountpoint}" = "/boot" -o "${mountpoint}" = "/boot/" ]; then
-      local boot="${devnode}"
+      boot="${devnode}"
     fi
   done
   if [ -z "${boot}" ]; then
-    local boot="${root}"
+    boot="${root}"
   fi
   echo "${boot}|${root}"
 }
 
 get_device_from_devnode() {
-  local devnode=$1
+  devnode=$1
 
 #  echo ${devnode} | sed -e 's:\(/[a-z]\+\)[0-9]\+$:\1:' -e 's:\(/[a-z]\+[0-9]\+\)p[0-9]\+$:\1:'
   echo ${devnode} | sed -e 's:p\?[0-9]\+$::'
 }
 
 get_device_and_partition_from_devnode() {
-  local devnode=$1
+  devnode=$1
 
   echo ${devnode} | sed -e 's:p\?\([0-9]\+\)$:|\1:'
 }
@@ -63,7 +63,7 @@ sanity_check_config_bootloader() {
   debug sanity_check_config_bootloader "no arch-specific bootloader config sanity check function"
 }
 
-local arch=$(get_arch)
+arch=$(get_arch)
 if [ -f "modules/bootloader_${arch}.sh" ]; then
   debug bootloader.sh "loading arch-specific module bootloader_${arch}.sh"
   import bootloader_${arch}
