f1569012f29baff0d366404580f41f1dd269f881
Lucas Ramage <ramage.lucas@protonmail.com>
2020-06-11 14:20:36 -0400

    Fix GitHub Actions for Vagrant

--- .local/share/quickstart/bin/provision.sh
+++ .local/share/quickstart/bin/provision.sh
@@ -2,11 +2,14 @@
 # SPDX-License-Identifier: GPL-2.0-only
 set -eu
 
+# Remove existing configuration
+rm -rf /etc/portage/package.accept_keywords
+
 # Copy portage configuration
 cp -R /usr/src/quickstart/.local/etc/portage/* /etc/portage/
 
 # Install dependencies
-emerge -ng @quickstart-dependencies
+emerge -nq @quickstart-dependencies
 
 # Set vi as default editor
 ln -s /bin/busybox /usr/local/bin/vi
